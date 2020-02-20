18189152bbca8e9590bd019ef4a0138882ac3925
Dmitry <dmitr.prot@gmail.com>
2020-02-20 23:52:59 +0300

    [PAY-1] Create application skeleton

---
+++ src/main/java/ru/yamoney/payments/PaymentsApplication.java
@@ -0,0 +1,16 @@
+package ru.yamoney.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Protsko Dmitry
+ */
+@SpringBootApplication
+public class PaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsApplication.class, args);
+    }
+
+}
